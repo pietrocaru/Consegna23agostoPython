6705d6d9a2af87b77c13f7277232862ea27d132d
Pietro Caruso <pietrocaruso22@gmail.com>
2024-08-23 11:41:31 +0200

    Add files via upload

---
+++ correzione_datetoday copy.py
@@ -0,0 +1,22 @@
+import datetime
+
+def assistente_virtuale(comando):
+    if comando == "Qual è la data di oggi?":
+        oggi = datetime.datetime.now().date()   # qui ho modificato il parametro datetoday() in modo da fargli avere senso
+        risposta = "La data di oggi è " + oggi.strftime("%d/%m/%Y")
+    elif comando == "Che ore sono?":
+        ora_attuale = datetime.datetime.now().time()
+        risposta = "L'ora attuale è " + ora_attuale.strftime("%H:%M")
+    elif comando == "Come ti chiami?":
+        risposta = "Mi chiamo Assistente Virtuale"
+    else:
+        risposta = "Non ho capito la tua domanda."  
+    return risposta
+
+while True:     # qui ho aggiunto i ":" dopo while True
+    comando_utente = input("Cosa vuoi sapere? ")
+    if comando_utente.lower() == "esci":
+        print("Arrivederci!")
+        break
+    else:
+        print(assistente_virtuale(comando_utente))
